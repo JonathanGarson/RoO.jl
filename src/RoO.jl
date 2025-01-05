015330f479a084213a432476c47619dbe464f3b5
MathildeB9 <mathblanchon@gmail.com>
2025-01-05 21:06:26 +0100

    restaured

--- src/RoO.jl
+++ src/RoO.jl
@@ -17,21 +17,15 @@ using Statistics
 
 # import all functions from the script 
 
-
 Pkg.instantiate()  # Install the dependencies listed in Project.toml
 
-# Main function to execute the package without simulations
 function run_short()
     include("src/AALA_clean.jl")                    # Cleans AALA data and outputs a file
-    include("src/AALA_IHS_table.jl")                # Generates Table 1
-    include("src/AALA_calibration_plots.jl")        # Generate plot 1
-end 
+end
 
-# 
 function run_long()
-    include("src/AALA_clean.jl")                    # Cleans AALA data and outputs a file
     include("src/AALA_IHS_table.jl")                # Generates Table 1
-    include("src/AALA_calibration_plots.jl")        # Generate plot 1
+    include("src/AALA_calibration_plots.jl")       # Generate plot 1
     include("AALA_solving_model_alternative.jl")
     include("AALA_grid_search_alternative.jl")
 end
