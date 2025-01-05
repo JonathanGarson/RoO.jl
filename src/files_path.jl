d532a6b76322312b0a6ddc13a951d780a87227db
JonathanGarson <garsonjonathan22@gmail.com>
2025-01-05 22:41:01 +0100

    files path

---
+++ src/files_path.jl
@@ -0,0 +1,11 @@
+# This document determine the file paths for the data and output files.
+
+# Path to the data file
+const input_raw_data = "data/AALA"
+
+# Path to the output files
+const output_data = "output/data"
+const output_figures = "output/figures"
+const output_tables = "output/tables"
+const output_parameters = "output/parameters"
+
