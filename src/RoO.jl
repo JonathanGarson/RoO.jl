898de8869a7b42522dd7cd19f27cfb1609ddb911
MathildeB9 <mathblanchon@gmail.com>
2025-01-05 15:40:20 +0100

    run_short

--- src/RoO.jl
+++ src/RoO.jl
@@ -19,6 +19,15 @@ using Statistics
 include("AALA_solving_model_alternative.jl")
 include("AALA_grid_search_alternative.jl")
 
+using Pkg
+Pkg.instantiate()  # Install the dependencies listed in Project.toml
+
+function run()
+    include("src/AALA_clean.jl")        # Cleans AALA data and outputs a file
+    include("src/AALA_IHS_table.jl")    # Generates Table 1
+    include("src/AALA_calibration_plots.jl") # Generate plot 1
+end 
+
 
 # Main function to execute the package
 
