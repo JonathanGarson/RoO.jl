664e856490ac853cc098b0c4da201f0bffa3f78e
Documenter.jl <documenter@juliadocs.github.io>
2025-01-07 10:54:06 +0000

    build based on c0875a7

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#RoO.jl","page":"RoO.jl","title":"RoO.jl","text":"","category":"section"},{"location":"","page":"RoO.jl","title":"RoO.jl","text":"Documentation for RoO.jl","category":"page"}]
+}
